fea20c11b9c78284b2505c0f8484073ba414e834
GwenRspl <gwenaelle.rispal@gmail.com>
2020-08-24 22:11:19 +0200

    Add slider to display time remaining before end of spawning

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour {
+
+    [Tooltip("Our level timer in SECONDS")]
+    [SerializeField] float levelTime = 10f;
+
+    private void Update() {
+        GetComponent<Slider>().value = Time.timeSinceLevelLoad / this.levelTime;
+        bool timerFinished = (Time.timeSinceLevelLoad >= this.levelTime);
+        if (timerFinished) {
+            Debug.Log("level timer finshed");
+        }
+    }
+
+}
