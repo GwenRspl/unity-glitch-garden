57a353e1c2a40e2dca1c66fca5e16610fa556638
GwenRspl <gwenaelle.rispal@gmail.com>
2020-08-28 16:15:22 +0200

    Implement volume slider

---
+++ Assets/OptionsController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour {
+
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] float defaultVolume = 0.8f;
+
+    private void Start() {
+        this.volumeSlider.value = PlayerPrefsController.GetMasterVolume();
+    }
+
+    private void Update() {
+        var musicPlayer = FindObjectOfType<MusicPlayer>();
+        if (musicPlayer) {
+            musicPlayer.SetVolume(this.volumeSlider.value);
+        } else {
+            Debug.LogWarning("No music player found");
+        }
+    }
+
+    public void SaveAndExit() {
+        PlayerPrefsController.SetMasterVolume(this.volumeSlider.value);
+        FindObjectOfType<LevelLoader>().LoadStartScreen();
+    }
+
+    public void SetDefaults() {
+        this.volumeSlider.value = this.defaultVolume;
+    }
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+    AudioSource audioSource;
+
+    private void Awake() {
+        SetUpSingleton();
+    }
+
+    private void SetUpSingleton() {
+        if (FindObjectsOfType(GetType()).Length > 1) {
+            Destroy(this.gameObject);
+        } else {
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    void Start() {
+        this.audioSource = GetComponent<AudioSource>();
+        this.audioSource.volume = PlayerPrefsController.GetMasterVolume();
+    }
+
+    public void SetVolume(float volume) {
+        this.audioSource.volume = volume;
+    }
+
+}
